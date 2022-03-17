b2fa26b68a33f679e156cd30e1c6222d4a91cbcb
Ramil <ra777@bk.ru>
2022-03-17 23:07:05 +0300

    add func

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function getRandomInt(min, max) {
+  if (min >= max) {
+    throw new Error('min cannot be greater than or equal to max');
+  }
+  if (min < 0 || max < 0) {
+    throw new Error('min and max cannot be less then 0');
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+getRandomInt();
+
+function checkStringLength (str, max) {
+  return str.length >= max;
+}
+
+checkStringLength();
